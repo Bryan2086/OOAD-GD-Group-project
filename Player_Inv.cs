1e464531af5dcdd8be72b5fb02294c641457daa8
Bryan2084 <bryanmc93@live.ie>
2015-12-14 11:32:29 +0000

    Added first iteration code
    Added the first iteration of code before any changes

---
+++ Player_Inv.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+public class Player_Inv : MonoBehaviour {
+
+    List<HealthPotion> list = new List<HealthPotion>();
+
+    HealthPotion hp = new HealthPotion();
+
+	// Use this for initialization
+	void Start () {
+        list.Add(hp);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RemoveItem(){
+
+        if (list.Contains(hp)) {
+            list.Remove(hp);
+            Debug.Log("inv");
+            GameObject.FindGameObjectWithTag("Player").SendMessage("usePotion");
+        }
+    }
+
+
+    public void AddItem() { list.Add(hp);  }
+}
---
+++ Player_Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+
+    public int speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W)) { MoveForward(); }
+
+        if (Input.GetKey(KeyCode.A)) { MoveLeft(); }
+
+        if (Input.GetKey(KeyCode.S)) { MoveBackward();  }
+
+        if (Input.GetKey(KeyCode.D)) { MoveRight(); }
+                                                                                          
+      
+    }
+
+    public void MoveForward() { transform.Translate(0, 0, speed * Time.deltaTime); }
+
+    public void MoveBackward() { transform.Translate(0, 0, -speed * Time.deltaTime); }
+
+    public void MoveLeft() { transform.Translate(-speed * Time.deltaTime, 0, 0); }
+
+    public void MoveRight() { transform.Translate(speed * Time.deltaTime, 0, 0); }
+}
