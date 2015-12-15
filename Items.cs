9732f93fa389329fec4ec632813386191f15a452
PaulNoonan19 <paulnoonan1993@gmail.com>
2015-12-15 14:21:26 +0000

    Added the buyValue and sellValue fields to the items class
    Added the buyValue and sellValue fields to the items class along with
    their getters and setters.

--- Items.cs
+++ Items.cs
@@ -4,14 +4,29 @@ using System.Collections;
 public class Items : MonoBehaviour {
     private int healAmount;
     private string name;
+    private int buyValue;
+    private int sellValue;
 
-    public Items(int healAmount, string name) {
+    public Items(int healAmount, string name, int buyValue, int sellValue) {
         this.healAmount = healAmount;
         this.name = name;
+        this.buyValue = buyValue;
+        this.sellValue = sellValue;
     }
 
     public void setHealAmount(int heal) { healAmount = heal; }
     public int getHealAmount() { return healAmount; }
+
+    public void setBuyValue(int buyValue) { this.buyValue = buyValue; }
+    public int getBuyValue()
+    {
+        return buyValue;
+    }
+    public void setValue(int value) { this.sellValue = value; }
+    public int getSellValue()
+    {
+        return sellValue;
+    }
 
     public string getName() {
         return name;
